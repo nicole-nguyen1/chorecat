dfeab8fa2c127d93ab085ad466011de8ebc51c47
Logan <hirelogan+github@gmail.com>
2018-07-18 10:19:20 -0500

    integrate calendar route with db functions

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const { completeChore } = require('../../database');
+const fetchAllCompletedChores = require('../../database').findAll('completedChores');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // Run get all users DB helper method as a promise, then res.send
-  res.json('GET to /calendar... Mock calendar data goes here...');
+  fetchAllCompletedChores()
+    .then(rows => res.status(200).json(rows))
+    .catch(err => console.error(`[error] GET calendar ${err}`));
 });
 
 router.post('/', (req, res) => {
-  // Run get all users DB helper method as a promise, then res.send
-  res.json('POST to /calendar... Mock calendar data goes here...');
+  const { choreId, userId, day } = req.body;
+  completeChore(userId, choreId, day)
+    .then(res.status(201).send())
+    .catch(err => console.error(`[error] POST calendar ${err}`));
 });
 
 module.exports = router;
