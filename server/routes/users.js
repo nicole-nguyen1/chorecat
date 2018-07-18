4f8a3c5c47d3ee960c605ecd25da5add9cdc3309
Logan <hirelogan+github@gmail.com>
2018-07-18 10:47:10 -0500

    enable all users verbs to work properly

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const { addUser, editUser, deleteUser } = require('../../database/');
-const fetchAllUsersFromDB = require('../../database/').findAll('user');
+const fetchAllUsersFromDB = require('../../database/').findAll;
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  fetchAllUsersFromDB()
+  fetchAllUsersFromDB('users')
     .then(rows => res.status(200).json(rows))
     .catch(err => console.error(`[error] GET users ${err}`));
 });
 
 router.post('/', (req, res) => {
-  addUser()
+  addUser(req.body.name)
     .then(success => res.status(201).json({ newUserId: success.insertId }))
     .catch(err => console.error(`[error] POST users ${err}`));
 });
 
 router.put('/:userId', (req, res) => {
-  editUser()
+  editUser(req.params.userId, req.body.name)
     .then(res.status(200).send())
     .catch(err => console.error(`[error] PUT user ${req.params.userId} --> ${err}`));
 });
 
 router.delete('/:userId', (req, res) => {
-  deleteUser()
+  deleteUser(req.params.userId)
     .then(res.status(200).send())
     .catch(err => console.error(`[error] DELETE user ${req.params.userId} --> ${err}`));
 });
