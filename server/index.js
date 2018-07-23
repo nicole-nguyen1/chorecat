b4bd547fa05137355125d49b79a92e93705e453f
Logan <hirelogan+github@gmail.com>
2018-07-23 16:00:16 -0500

    fix secret

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const passport = require('passport');
 const { Strategy } = require('passport-local');
 const { findUser } = require('../database/');
 
+const secret = process.env.AUTH_KEY;
+
 // Routes
 const users = require('./routes/users');
 const chores = require('./routes/chores');
@@ -18,7 +20,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../client/dist/')));
 app.use(bodyParser.json()); // Expect body to always be JSON
-app.use(session({ secret: 'keyboard cats', resave: false, saveUninitialized: false }));
+app.use(session({ secret, resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/users', users); // Setup route
