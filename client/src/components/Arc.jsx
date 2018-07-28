f33428573ab0c1a66acad463fb844171d30917a9
Jeffrey Chea <jeffreychea234@gmail.com>
2018-07-28 17:12:24 -0500

    corrected chart function

--- client/src/components/Arc.jsx
+++ client/src/components/Arc.jsx
@@ -24,34 +24,13 @@ class LabeledArc extends Arc {
       this.arc.innerRadius(this.props.innerRadius);
       this.arc.outerRadius(this.props.outerRadius);
       this.arc.cornerRadius(this.props.cornerRadius);
-      this.state = {
-        toolTip: ""
-      }
-      this.toolTipCreator = this.toolTipCreator.bind(this);
+
+
     }
-    componentDidMount(){
-        console.log(this.props.data, 'hello')
-        this.toolTipCreator();
-    }
-    toolTipCreator(){
-      this.setState({
-        toolTip: `${this.props.data.data.label} completed (${this.props.data.data.value}) chores`
-      })
-    }
-    //
-    // componentDidUpdate(){
-    //   d3.select(this)
-    //     .append("svg:title")
-    //     .text(this.state.toolTip)
-    //     .style("display", "block")
-    // }
+
 
 
     render() {
-      d3.select(this)
-        .append("svg:title")
-        .text(this.state.toolTip)
-        .style("display", "block");
         let [labelX, labelY] = this.arc.centroid(this.props.data),
             labelTranslate = `translate(${labelX}, ${labelY})`;
 
